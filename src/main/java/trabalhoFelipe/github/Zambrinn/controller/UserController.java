bfe4064c24cb09150ee1f483ffa91a931e98a3d2
Zambrinn <zambrinfilho@gmail.com>
2025-11-22 17:42:07 -0300

    refactor: reorganize imports and enhance getUserById method in UserController

--- src/main/java/trabalhoFelipe/github/Zambrinn/controller/UserController.java
+++ src/main/java/trabalhoFelipe/github/Zambrinn/controller/UserController.java
@@ -1,16 +1,23 @@
 package trabalhoFelipe.github.Zambrinn.controller;
 
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import trabalhoFelipe.github.Zambrinn.model.DTOs.UserRequest;
-import trabalhoFelipe.github.Zambrinn.model.DTOs.UserResponse;
-import trabalhoFelipe.github.Zambrinn.model.User;
-import trabalhoFelipe.github.Zambrinn.service.UserService;
-
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import trabalhoFelipe.github.Zambrinn.model.DTOs.UserRequest;
+import trabalhoFelipe.github.Zambrinn.model.DTOs.UserResponse;
+import trabalhoFelipe.github.Zambrinn.service.UserService;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -21,6 +28,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserResponse>> getUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @PostMapping
